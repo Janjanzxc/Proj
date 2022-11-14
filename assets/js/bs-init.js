599ed4c8df7886b21a86754c22bd08e4a84bb623
Janjan Lucero <dummies215@gmail.com>
2022-11-14 15:50:15 +0800

    commit on master

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,4 +1,41 @@
 
+import myJson from './Student.json' assert {type: 'json'};
+(function (){
+	//Global Variables
+
+	//Get Form Value's
+	document.getElementById("form").addEventListener("submit", e =>{
+	var studentEmail =document.getElementById("emailInput")
+	var studentPass = document.getElementById("passInput")
+	
+		e.preventDefault();
+
+		validate(studentEmail.value,studentPass.value);
+		studentEmail.value = "";
+		studentPass.value = "";
+		console.log(studentEmail);
+	});
+
+	function validate(email,pass){
+		// filter json data
+		var _myStudent_data = myJson.filter(element => element.Email.toUpperCase() == email.toUpperCase());
+		if (_myStudent_data.length == 0){
+			alert("No user found");
+		}
+		 else {
+			for(var i of _myStudent_data){
+				if(i.Password == pass){
+					alert("Password match");
+				}
+				else {
+					alert("Password does not Match")
+				}
+			}
+		 }
+	}
+})();
+
+
 if (window.innerWidth < 768) {
 	[].slice.call(document.querySelectorAll('[data-bss-disabled-mobile]')).forEach(function (elem) {
 		elem.classList.remove('animated');
